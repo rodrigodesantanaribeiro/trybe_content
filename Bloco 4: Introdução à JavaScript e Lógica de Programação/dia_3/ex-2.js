1e564c4e6ab14e133c3ca3bd769b206835d15652
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-10-11 15:06:13 -0300

    dia_3_exercícios

---
+++ Bloco 4: Introdução à JavaScript e Lógica de Programação/dia_3/ex-2.js
@@ -0,0 +1,10 @@
+
+let size = 5;
+let lineIndex;
+let symbol = '*';
+let inputLine = '';
+
+for (lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+  console.log(inputLine);
+  inputLine = inputLine + symbol;
+};
